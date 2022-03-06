4276c05a534a5e29bc135d15ee0c6dc5a97caa81
choesuhong <57895652+choesuhong@users.noreply.github.com>
2022-03-06 13:06:20 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 68 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -0,0 +1,18 @@
+import sys;
+def solution():
+    input = sys.stdin.readline
+
+    dp = [0]*11
+
+    dp[1] = 1
+    dp[2] = 2
+    dp[3] = 4
+
+    # print(set(dp[0]))
+    for i in range(4,11):
+        dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+
+    for _ in range(int(input())):
+        print(dp[int(input())])
+
+solution()
