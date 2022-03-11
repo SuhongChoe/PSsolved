dbc48abd244e52e5693d992892649cb28a0efa12
choesuhong <57895652+choesuhong@users.noreply.github.com>
2022-03-11 14:35:23 +0900

    [Silver III] Title: 01타일, Time: 160 ms, Memory: 30864 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.py
+++ 백준/Silver/1904. 01타일/01타일.py
@@ -5,9 +5,8 @@ def solution():
 
     a = 1
     b = 2
-    n = int(input())
-    c = 0 if n>2 else n
-    for i in range(3,n+1):
+    c = int(input())
+    for i in range(3,c+1):
         c = (a + b) % 15746
         a, b = b, c
 
