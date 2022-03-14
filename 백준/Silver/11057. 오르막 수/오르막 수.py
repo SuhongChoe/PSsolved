f246f7063c1177a130816d3b0d98a6a353d17f84
choesuhong <57895652+choesuhong@users.noreply.github.com>
2022-03-14 19:14:39 +0900

    [Silver I] Title: 오르막 수, Time: 76 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Silver/11057. 오르막 수/오르막 수.py
@@ -0,0 +1,18 @@
+import sys
+
+def solution():
+    input = sys.stdin.readline
+
+    dp = [1]*10
+
+    n = int(input())
+
+    for _ in range(n-1):
+        new_dp = dp[:]
+        for i in range(10):
+            new_dp[i] = sum(map(lambda x : x%10007,dp[:i+1]))
+        dp = new_dp
+
+    print(sum(dp)%10007)
+
+solution()
