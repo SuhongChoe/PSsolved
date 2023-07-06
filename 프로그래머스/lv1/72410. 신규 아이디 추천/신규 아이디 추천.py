23ebbddc490a14acec67e3ec60ee8ae0007dce28
Suhong Choe <57895652+SuhongChoe@users.noreply.github.com>
2023-07-06 21:12:45 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.22 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -0,0 +1,34 @@
+def solution(new_id):
+    # 아이디 추천
+    # 아이디 규칙
+        # 3~15자 길이
+        # 알파벳 소문자, 숫자, -, _, . 등 의 문자 가능
+        # 처음과 끝은 .을 사용할 수 없으며 또한 연속으로 사용 불가
+        
+    # 치환단계
+    # 1. 대문자 -> 소문자
+    # 2. 알파벳 소문자, 숫자, -, _, .을 제외한 문자 제거
+    # 3. .가 2번 이상 연속된 부분을 하나의 마침표로 치환
+    # 4. 처음과 끝에 .이 있다면 제거
+    # 5. 빈 문자열이라면 a 대입
+    # 6. 길이가 16이상이라면 첫 15개를 제외한 문제 제거 (단, 이후 발생한 .가 있다면 추가 제거)
+    # 7. 길이가 2자 이하면 마지막 문자를 길이가 3이 될때까지 반복해서 복사
+    
+    new_id = new_id.lower() # 1번
+    
+    import re
+    new_id = re.sub(r'[^a-z0-9_.-]', '', new_id) # 2번
+    
+    new_id = re.sub(r'\.{2,}', '.', new_id) # 3번
+    
+    if new_id and new_id[0]=='.': new_id = new_id[1:]
+    if new_id and new_id[-1]=='.': new_id = new_id[:-1] # 4번
+    
+    if not new_id: new_id = 'a' # 5번
+    
+    if len(new_id)>15: new_id = new_id[:15] # 6번
+    if new_id and new_id[-1]=='.': new_id = new_id[:-1]
+    
+    while len(new_id)<3: new_id += new_id[-1] # 7번
+    
+    return new_id[:15]
