c71574a437807825bca3048270960e4f8a7c9c95
Suhong Choe <57895652+SuhongChoe@users.noreply.github.com>
2023-07-08 01:05:05 +0900

    [Silver I] Title: 숨바꼭질, Time: 124 ms, Memory: 34160 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,17 +1,61 @@
-import sys
-def solution():
-    input = sys.stdin.readline
+from sys import stdin, stdout
+input=stdin.readline
+print=stdout.write
+
+def function():
+    # write down code
+    # 일직선 상에서 수빈이와 동생이 위치함
+    # 수빈 -> n
+    # 동생 -> k
+    # 수빈이가 동생을 찾아야 함
+    # 방법 두가지 떠오름
+    # 둘다 십만개 이기 떄문에
+    # dfs로 풀면 게속 반복함
+    # 따라서 bfs로 풀거나
+    # dp로 풀거나
 
     n, k = map(int, input().split())
+    # dp로 풀 경우
+    # import sys
+    #
+    # dp = [sys.maxsize]*100001
+    # dp[n] = 0
+    # for i in range(n-1, -1, -1):
+    #     dp[i] = dp[i+1] + 1
+    #
+    # for i in range(100001):
+    #     tmp = sys.maxsize
+    #     if 0 <= i+1 <=100000:
+    #         tmp = min(tmp, dp[i+1])
+    #     if 0 <= i-1 <=100000:
+    #         tmp = min(tmp, dp[i-1])
+    #     if i%2==0:
+    #         tmp = min(tmp, dp[i//2])
+    #     dp[i] = min(dp[i], tmp + 1)
+    #
+    # return dp
 
-    dp = [float('inf')]*100001
-    for i in range(n+1): dp[n-i]=i
+    # bfs로 풀 경우
+    from collections import deque
+    q = deque([(n, 0)])
+    visited = [False]*100001
+    visited[n] = True
 
-    for i in range(1,100001):
-        a = dp[i//2]+1 if i%2==0 else float('inf')
-        d = dp[(i+1)//2]+2 if i%2==1 else float('inf')
-        b = dp[i-1]+1 if i-1>=0 else float('inf')
-        c = dp[i+1]+1 if i+1<=100000 else float('inf')
-        dp[i]= min(a,b,c,d,dp[i])
-    print(dp[k])
-solution()
+    while q:
+        new_n, cnt = q.popleft()
+
+        if new_n == k:
+            return cnt
+
+        if 0 <= new_n - 1 <= 100000 and not visited[new_n - 1]:
+            q.append((new_n - 1, cnt + 1))
+            visited[new_n - 1] = True
+        if 0 <= new_n + 1 <= 100000 and not visited[new_n + 1]:
+            q.append((new_n + 1, cnt + 1))
+            visited[new_n + 1] = True
+        if 0 <= new_n * 2 <= 100000 and not visited[new_n * 2]:
+            q.append((new_n * 2, cnt + 1))
+            visited[new_n * 2] = True
+
+if __name__ == "__main__":
+    print(str(function()))
