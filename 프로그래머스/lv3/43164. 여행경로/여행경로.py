3dfc661f5c9a9936f2d3f833a48b3edd85a7aa50
Suhong Choe <57895652+SuhongChoe@users.noreply.github.com>
2023-07-15 19:23:03 +0900

    [level 3] Title: 여행경로, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv3/43164. 여행경로/여행경로.py
+++ 프로그래머스/lv3/43164. 여행경로/여행경로.py
@@ -1,35 +1,27 @@
 def dfs(graph, node, res):
     while graph[node]:
         next_node = graph[node].pop()
-        dfs(graph, next_node, res)
+        res.append(dfs(graph, next_node, res))
     
-    res.append(node)
+    return node
+    
 
 def solution(tickets):
-    # 모든 공항은 알파벳 대문자 3글자
-    # 3 <= airport <= 10000
-    # [a, b] == a -> b 가는 항공권이라는 거임
-    # 주어진 항공권은 모두 사용하면서 경로가 여러개이면 사전이 빠른순으로 b를 정해서 가야함
-    # 중복이 허용이 됨 -> visited 필요없음
-    # 다만 돌아갈 길이 있는지는 확인해야할듯 -> 백트래킹 문제인듯????
-    
-    # solution 내가 문제 해석을 못함
-    # 모든 도시를 방문할 수 있다했으니 어떻게든 한줄로 이어져 있으며 순환경로가 없다는 뜻임
-    # 그냥 완전탐색 하면 되는 문제
+    # 주어진 항공권을 모두 사용하여 경로를 탐색
+    # 모든 도시를 방문할 수 없는 경우는 없음 -> 어떻게든 한줄로 이어진다는 뜻
+    # graph를 형성하고 ICN부터 방문하여 dfs하면됨
     
     from collections import defaultdict
     
     graph = defaultdict(list)
-    for a, b in tickets: 
+    
+    for a, b in tickets:
         graph[a].append(b)
-    
-    for k, _ in graph.items(): 
-        graph[k].sort(reverse=True)
-    
-    start = "ICN"
+    for k, _ in graph.items():
+        graph[k].sort(reverse=True) # 알파벳 순으로 탐색하기 위함
     
     res = []
+
+    dfs(graph, "ICN", res)
     
-    dfs(graph, start, res)
-    
-    return res[::-1]
+    return ["ICN"]+res[::-1]
