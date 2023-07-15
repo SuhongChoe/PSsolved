3f1efc14c9de1586c5cdd6a8522d371684c1c360
Suhong Choe <57895652+SuhongChoe@users.noreply.github.com>
2023-07-15 18:14:25 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -1,37 +1,24 @@
 def solution(bridge_length, weight, truck_weights):
-    # 순차적 일 작업을 해야함
-    # 다리에는 트럭이 최대 bridge_length대 만큼 and
-    # weight이하까지 트럭의 무게를 견딜 수 있음
-    # 모든 트럭이 다리를 건너는 최소 시간을 return
-    # 1 <= bridge_length <= 10000
-    # 1 <= weight <= 10000
-    # 1 <= truck_weights <= 10000
-    # 1 <= truck_weights[i] <= weight
-    
-    # deque를 활용하여 다리의 무게나 개수가 초과한다면 조건이 될 때까지 트럭을 뺴는걸로 구현해야할듯
-    
-    # solution 
-    # 0으로 채워서 하는게 10000*10000이라 1억 연산이라 괜찮은 방법이었음
-    # 최적화가 더 가능할 것 같지만 복잡함
+    # 트럭은 최대 bridge_length대 and weight 이하 무게까지 견딜 수 있음
+    # return 모든 트럭이 다리를 건너려면 최소 몇 초가 걸리는지
     
     from collections import deque
     
-    crossing_trucks = deque([0]*bridge_length)
-    bridge_weights = 0
+    bridge = deque([0]*bridge_length)
     truck_weights.reverse()
-    time = 0
+    truck_on_bridge_w = 0
+    res = 0
     
     while truck_weights:
-        bridge_weights -= crossing_trucks.pop()
-        if bridge_weights + truck_weights[-1] <= weight: # 트럭 보낼 수 있는 경우
+        truck_on_bridge_w -= bridge.popleft()
+        if truck_on_bridge_w + truck_weights[-1] > weight:
+            bridge.append(0)
+        else: # 건널 수 있을 땐
             truck = truck_weights.pop()
-            bridge_weights += truck
-            crossing_trucks.appendleft(truck)
-        else: # 트럭 내보낼 수 없는 경우
-            crossing_trucks.appendleft(0) # shift 처리
-        time += 1
-        
-    # 무조건 다리 첫번째 자리에 트럭이 오기때문에 다리 길이만큼 트럭 이동시간이 추가됨
-    time += bridge_length
+            truck_on_bridge_w += truck
+            bridge.append(truck)
+        res += 1
     
-    return time
+    res += bridge_length
+    
+    return res
