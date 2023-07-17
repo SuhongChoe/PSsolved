bd463ab40c3c3d32e03528b9a73f966c5832cd21
Suhong Choe <57895652+SuhongChoe@users.noreply.github.com>
2023-07-18 01:59:09 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -1,22 +1,21 @@
 def solution(bridge_length, weight, truck_weights):
-    # 트럭은 최대 bridge_length대 and weight 이하 무게까지 견딜 수 있음
-    # return 모든 트럭이 다리를 건너려면 최소 몇 초가 걸리는지
+    # 최악일때 만*만 이라서 1억번임 ㄱㅊ
     
     from collections import deque
     
-    bridge = deque([0]*bridge_length)
+    bridge = deque([0]*bridge_length) # bridge_length 만큼 다리 생성
     truck_weights.reverse()
-    truck_on_bridge_w = 0
+    total_weights = 0
     res = 0
     
     while truck_weights:
-        truck_on_bridge_w -= bridge.popleft()
-        if truck_on_bridge_w + truck_weights[-1] > weight:
-            bridge.append(0)
-        else: # 건널 수 있을 땐
+        total_weights -= bridge.popleft()
+        if total_weights + truck_weights[-1] <= weight:
             truck = truck_weights.pop()
-            truck_on_bridge_w += truck
             bridge.append(truck)
+            total_weights += truck
+        else:
+            bridge.append(0)
         res += 1
     
     res += bridge_length
