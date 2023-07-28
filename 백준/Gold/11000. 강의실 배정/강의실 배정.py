13a962c2a0ebb9c82e8c2c3ba521fd4787e99321
Suhong Choe <57895652+SuhongChoe@users.noreply.github.com>
2023-07-28 15:50:08 +0900

    [Gold V] Title: 강의실 배정, Time: 344 ms, Memory: 54544 KB -BaekjoonHub

---
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.py
@@ -0,0 +1,33 @@
+from sys import stdin, stdout
+input=stdin.readline
+print=stdout.write
+
+def function():
+    # write down code
+    # n -> 수업 갯수
+    # s t -> 수업시간
+
+    # 누적합이면 좋을 듯
+
+    from collections import defaultdict
+
+    dic = defaultdict(int)
+
+    for _ in range(int(input())):
+        s, t = map(int, input().split())
+        dic[s] += 1
+        dic[t] -= 1
+
+    keys = sorted(dic.keys())
+
+    res = 0
+    classroom = 0
+
+    for k in keys:
+        classroom += dic[k]
+        res = max(res, classroom)
+
+    return str(res)
+    
+if __name__ == "__main__":
+    print(function())
