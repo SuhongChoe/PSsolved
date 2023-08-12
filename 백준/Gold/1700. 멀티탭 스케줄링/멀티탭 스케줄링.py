90db19587e2de5223136fd147d30083515256b23
Suhong Choe <57895652+SuhongChoe@users.noreply.github.com>
2023-08-12 18:16:20 +0900

    [Gold I] Title: 멀티탭 스케줄링, Time: 64 ms, Memory: 34128 KB -BaekjoonHub

--- 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.py
+++ 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.py
@@ -5,44 +5,45 @@ print=stdout.write
 def function():
     # write code down
     # input
-    # 1. n 멀티탭 구멍 개수 1 <= n <= 100
-    # 2. k 전기 용품 개수 1 <= k <= 100
+    # 1. n, k : 멀티탭구멍, 용품 갯수 1 <= n <= 100 , 1 <= k <= 100
+    # 2. 멀티탭 정수 이름
 
     # output
-    # 최소한의 플러그를 빼는 횟수
+    # 플러그를 뺴는 최소한의 횟수
 
     # solution
-    # 다음과 같은 3가지 경우가 발생
-    # 1. 콘센트에 빈자리가 있는 경우
-    # 2. 이미 꽂혀있는 경우
-    # 3. 새로운 전기용품을 꽂아야하는 경우
+    # 세 가지 경우가 있다.
+    # 1. 구멍이 비어있는 경우
+    # 2. 이미 끼워져 있는 경우
+    # 3. 바꾸어야 할 경우
 
-    from collections import deque, defaultdict
+    from collections import defaultdict, deque
 
     n, k = map(int, input().split())
     arr = list(map(int, input().split()))
-
     dic = defaultdict(deque)
-    for i, v in enumerate(arr):
-        dic[v].append(i)
+    for id, el in enumerate(arr):
+        dic[el].append(id)
+    plug = set()
 
     res = 0
 
-    plug = set([])
-
-    for elect in arr:
+    for i in range(k):
+        elect = arr[i]
         if len(plug) == n and elect not in plug:
-            last = 0
-            off = 0
-            for old_elect in plug:
-                if not dic[old_elect]:
-                    off = old_elect
+            latest, off_plug = 0, 0
+
+            for on_plug in plug:
+                if not dic[on_plug]:
+                    off_plug = on_plug
                     break
-                if last < dic[old_elect][0]:
-                    last = dic[old_elect][0]
-                    off = old_elect
-            plug.remove(off)
+                if latest < dic[on_plug][0]: # 가장 마지막에 나타날 플러그를 뽑아야 함
+                    latest = dic[on_plug][0]
+                    off_plug = on_plug
+
+            plug.remove(off_plug)
             res += 1
+
         plug.add(elect)
         dic[elect].popleft()
 
