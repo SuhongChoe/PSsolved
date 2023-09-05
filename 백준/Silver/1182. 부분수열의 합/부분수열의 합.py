64aec7689a81d57efa355e2ddd2b185012e88cb8
Suhong Choe <57895652+SuhongChoe@users.noreply.github.com>
2023-09-06 00:25:41 +0900

    [Silver II] Title: 부분수열의 합, Time: 268 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -10,54 +10,39 @@ print=stdout.write
 #     dfs(arr, idx+1, s-arr[idx], res, n)
 #     dfs(arr, idx+1, s, res, n)
 
-# def function():
-#     # write down code
-#     # 1. n, s : 정수의 개수, 부분수열 중 원소를 다 더한 값 s, 1 <= n <= 20, |s| <= 1000000
-#     # 2. arr
-
-#     # solution
-
-#     res = [0]
-
-#     n, s = map(int, input().split())
-#     arr = list(map(int, input().split()))
-#     arr.sort()
-
-#     dfs(arr, 0, s, res, n)
-
-#     return str(res[0])
-
 def function():
     # write code down
-    # 부분적으로 원소를 더했을 때 S가 되는 경우의 수 출력
+    # 수열 안의 원소들의 합으로 s를 만들 수 있는 경우의 수
 
     # input
-    # 1. n, s : 정수의 개수, target (1 <= n <= 20, |s| <= 1000000)
-    # 2. arr : 주어진 배열 (|arr[i]| <= 100000)
-    
+    # 1. n, s : 정수의 개수, 만들고자하는 수 (1 <= n <= 20, |S| <= 1000000)
+    # 2. arr : 수열 ( |arr[i]| <= 100000)
+
     # output
-    # s가 되는 경우의 수
+    # 1. 만들 수 있는 부분수열의 개수
 
     n, s = map(int, input().split())
     arr = list(map(int, input().split()))
     arr.sort()
+
     res = [0]
 
-    dfs(arr, 0, n, s, res)
+    backtracking(arr, s, 0, n, res)
 
     return str(res[0])
 
-def dfs(arr, i, n, s, res):
+def backtracking(arr, s, i, n, res):
     if i == n:
         return
-        
+
     s -= arr[i]
-    
-    if s==0:
+
+    if s == 0:
         res[0] += 1
-    
-    dfs(arr, i+1, n, s+arr[i], res)
-    dfs(arr, i+1, n, s, res)
+
+    backtracking(arr, s+arr[i], i+1, n, res)
+    backtracking(arr, s, i+1, n, res)
+
 
 if __name__ == "__main__":
     print(function())
