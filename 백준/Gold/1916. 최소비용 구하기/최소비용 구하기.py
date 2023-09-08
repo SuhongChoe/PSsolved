31e09b7a6d4ee3cf62373a597d8e640caa870df4
Suhong Choe <57895652+SuhongChoe@users.noreply.github.com>
2023-09-09 00:06:46 +0900

    [Gold V] Title: 최소비용 구하기, Time: 284 ms, Memory: 57680 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -1,44 +1,95 @@
 import sys
-input=sys.stdin.readline
-# print=sys.stdout.write
+input = sys.stdin.readline
+print = sys.stdout.write
+
+# def function():
+#     # write code down
+#
+#     from collections import defaultdict
+#
+#     n = int(input())
+#     m = int(input())
+#     graph = defaultdict(list)
+#     for _ in range(m):
+#         a, b, distance = map(int, input().split())
+#         graph[a].append((distance, b))
+#     start, end = map(int, input().split())
+#
+#     d = {i:sys.maxsize for i in range(1, n+1)}
+#     d[start] = 0
+#
+#     dijkstra(graph, d, start)
+#
+#     return str(d[end])
+#
+# def dijkstra(graph, d, start):
+#     import heapq
+#
+#     heap = [(d[start],start)]
+#
+#     while heap:
+#         distance, node = heapq.heappop(heap)
+#
+#         if d[node] < distance:
+#             continue
+#
+#         for cost, next_node in graph[node]:
+#             new_cost = cost + distance
+#             if d[next_node] > new_cost:
+#                 d[next_node] = new_cost
+#                 heapq.heappush(heap, (new_cost, next_node))
 
 def function():
-    # write down code
+    # write code down
+    # 도시의 번호 1~n
+
+    # input
+    # 1. n : 도시의 개수 (1 <= n <= 1000)
+    # 2. m : 버스의 개수 (1 <= m <= 100000)
+    # 3~m+2. a, b, distance : 비용 (0 <= distance < 100000)
+    # m+3. start, end : 출발점 도착점 도시 번호
+
+    # output
+    # 출발점에서 도착점까지의 최소비용
+    global n, m, graph, costs
 
     from collections import defaultdict
+
     n = int(input())
     m = int(input())
     graph = defaultdict(list)
     for _ in range(m):
-        a, b, cost = map(int, input().split())
-        graph[a].append((cost, b))
+        a, b, distance = map(int, input().split())
+        graph[a].append((distance, b))
 
     start, end = map(int, input().split())
-    costs = {i:sys.maxsize for i in range(1, n+1)}
+
+    costs = [1e8]*(n+1)
     costs[start] = 0
 
-    dijkstra(start, graph, costs)
+    dijkstra(start)
 
     return str(costs[end])
 
-def dijkstra(start, graph, costs):
+def dijkstra(start):
+    global n, m, graph, costs
+
     import heapq
-    heap = []
 
-    heapq.heappush(heap, (costs[start], start))
+    q = [(0, start)]
 
-    while heap:
-        cost, node = heapq.heappop(heap)
+    while q:
+        distance, node = heapq.heappop(q)
 
-        if costs[node] < cost:
+        if distance > costs[node]:
             continue
 
-        for next_cost, next_node in graph[node]:
-            nc = cost+next_cost
-            if costs[next_node] > nc:
-                costs[next_node] = nc
-                heapq.heappush(heap, (nc, next_node))
+        for next_distance, next_node in graph[node]:
+            new_distance = distance + next_distance
 
+            if costs[next_node] > new_distance:
+                costs[next_node] = new_distance
+                heapq.heappush(q, (new_distance, next_node))
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     print(function())
