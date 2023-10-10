0b76aaeb7080f3f63a776a2971b19928ef475b72
Suhong Choe <57895652+SuhongChoe@users.noreply.github.com>
2023-10-10 17:41:29 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 428 ms, Memory: 115516 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -2,92 +2,103 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    // static int[] parent;
+    static int parents [];
     
-    // public static int find(int x){
-    //     if (parent[x] == x) return x;
-    //     return parent[x] = find(parent[x]);
-    // }
-
-    // public static void union(int aRoot, int bRoot){
-    //     if (aRoot > bRoot) parent[aRoot] = bRoot;
-    //     else parent[bRoot] = aRoot;
-    // }
-    
-    // public static void main(String[] args) throws IOException {
-    //     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    //     StringTokenizer st = new StringTokenizer(br.readLine());
-
-    //     HashSet<Integer> s = new HashSet<>();
-
-    //     int n = Integer.parseInt(st.nextToken());
-    //     int m = Integer.parseInt(st.nextToken());
-
-    //     parent = new int[n+1];
-
-    //     for(int i=1; i<parent.length; i++) parent[i] = i;
-
-    //     for(int i=0; i<m; i++){
-    //         st = new StringTokenizer(br.readLine());
-    //         int a = Integer.parseInt(st.nextToken());
-    //         int b = Integer.parseInt(st.nextToken());
-
-    //         int aRoot = find(a);
-    //         int bRoot = find(b);
-            
-    //         if(aRoot!=bRoot) union(aRoot, bRoot);
-    //     }
-
-    //     for(int i=1; i<parent.length; i++) s.add(parent[i]);
-
-    //     System.out.print(s.size());
-    // }
-
-    static boolean visited[];
-    static ArrayList<Integer>[] arr;
-
-    public static void DFS(int node){
-        visited[node] = true;
-        for(int next_node : arr[node]){
-            if (!visited[next_node]){
-                DFS(next_node);
-            }
+    public static int find(int x){
+        if (parents[x] == x)
+            return x;
+        else{
+            return parents[x] = find(parents[x]);
         }
     }
 
+    public static void union(int a, int b){
+        int aRoot = find(a);
+        int bRoot = find(b);
+        
+        if (aRoot > bRoot){
+            parents[aRoot] = bRoot;
+        } else if (aRoot < bRoot) {
+            parents[bRoot] = aRoot;
+        } else{
+            return;
+        }
+    }
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
+        HashSet<Integer> s = new HashSet<>();
+
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        visited = new boolean[n+1];
-        arr = new ArrayList[n+1];
 
-        for(int i=1; i<n+1; i++){
-            arr[i] = new ArrayList<Integer>();
-        }
+        parents = new int[n+1];
+
+        for(int i=1; i<parents.length; i++) parents[i] = i;
 
         for(int i=0; i<m; i++){
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
 
-            arr[a].add(b);
-            arr[b].add(a);
+            int aRoot = find(a);
+            int bRoot = find(b);
+
+            union(aRoot, bRoot);
         }
 
-        int count = 0;
-
-        for(int i=1; i<n+1; i++){
-            if (!visited[i]){
-                DFS(i);
-                count++;
-            }
+        for(int i=1; i<parents.length; i++) {
+            find(i);
+            s.add(parents[i]);
         }
 
-        System.out.print(count);
-
+        System.out.print(s.size());
     }
 
+    // static boolean visited[];
+    // static ArrayList<Integer> arr[];
+
+    // public static void DFS(int node){
+    //     visited[node] = true;
+    //     for(int next_node : arr[node]){
+    //         if (!visited[next_node]){
+    //             DFS(next_node);
+    //         }
+    //     }
+    // }
+
+    // public static void main(String[] args) throws IOException {
+    //     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    //     StringTokenizer st = new StringTokenizer(br.readLine());
+
+    //     int n = Integer.parseInt(st.nextToken());
+    //     int m = Integer.parseInt(st.nextToken());
+    //     visited = new boolean[n+1];
+    //     arr = new ArrayList[n+1];
+
+    //     for(int i=1; i<n+1; i++){
+    //         arr[i] = new ArrayList<Integer>();
+    //     }
+
+    //     for(int i=0; i<m; i++){
+    //         st = new StringTokenizer(br.readLine());
+    //         int a = Integer.parseInt(st.nextToken());
+    //         int b = Integer.parseInt(st.nextToken());
+
+    //         arr[a].add(b);
+    //         arr[b].add(a);
+    //     }
+
+    //     int count = 0;
+
+    //     for(int i=1; i<n+1; i++){
+    //         if (!visited[i]){
+    //             DFS(i);
+    //             count++;
+    //         }
+    //     }
+    //     System.out.print(count);
+    // }
 }
