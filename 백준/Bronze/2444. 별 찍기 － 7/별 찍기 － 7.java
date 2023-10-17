6ecfa2178900ff6cef7d83ce2cf386fafc77fe8f
Suhong Choe <57895652+SuhongChoe@users.noreply.github.com>
2023-10-17 17:45:57 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 84 ms, Memory: 11848 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        StringBuffer sb = new StringBuffer();
+
+        for(int i=1;i<=n; i++){
+            for(int j=1; j<=n-i; j++) sb.append(" ");
+            for(int j=1; j<=2*i-1; j++) sb.append("*");
+            sb.append("\n");
+        }
+
+        for(int i=1;i<=n-1; i++){
+            for(int j=1; j<=i; j++) sb.append(" ");
+            for(int j=1; j<=2*(n-i)-1; j++) sb.append("*");
+            sb.append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
