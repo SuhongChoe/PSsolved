00453afb619a0d3acd8e581f13dc95cedfccc1fd
Suhong Choe <57895652+SuhongChoe@users.noreply.github.com>
2023-10-20 11:32:01 +0900

    [level 2] Title: 타겟 넘버, Time: 0.38 ms, Memory: 65.8 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,19 +1,12 @@
 class Solution {
-    static int res = 0;
-    
-    public static void dfs(int i, int[] numbers, int target){
+    public static int dfs(int i, int[] numbers, int target){
         if(i==numbers.length){
-            if(target==0) res++;
-            return;
+            return target==0 ? 1 : 0;
         }
-        dfs(i+1, numbers, target-numbers[i]);
-        dfs(i+1, numbers, target+numbers[i]);
+        return dfs(i+1, numbers, target-numbers[i]) + dfs(i+1, numbers, target+numbers[i]);
     }
     
     public int solution(int[] numbers, int target) {
-        
-        dfs(0,numbers, target);
-        
-        return res;
+        return dfs(0,numbers, target);
     }
 }
