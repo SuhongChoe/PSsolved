a61bbf7bdc747fbdbe08ed75d84f8373ca64f327
Suhong Choe <57895652+SuhongChoe@users.noreply.github.com>
2023-10-20 10:06:55 +0900

    [level 2] Title: 주식가격, Time: 23.70 ms, Memory: 63.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public int[] solution(int[] prices) {
+        Deque<int[]> dq = new LinkedList<>();
+        ArrayList<int[]> ps = new ArrayList<>();
+        
+        for(int i=0; i<prices.length; i++){
+            ps.add(new int[] {i, prices[i]}); // idx, value
+        }
+        
+        int res[] = new int[ps.size()];
+        
+        for(int i=0;i<ps.size(); i++){
+            while(!dq.isEmpty() && dq.peekLast()[1] > ps.get(i)[1]){
+                int idx = dq.pollLast()[0];
+                res[idx] = i-idx;
+            }
+            dq.addLast(ps.get(i));
+        }
+        
+        while(!dq.isEmpty()){
+            int idx = dq.pollLast()[0];
+            res[idx] = ps.size() - 1 - idx;
+        }
+        
+        return res;
+    }
+}
