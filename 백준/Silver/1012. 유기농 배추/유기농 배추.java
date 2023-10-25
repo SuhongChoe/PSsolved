e9b685f12148e39023bed2a8a2df3ffafa9f01a9
Suhong Choe <57895652+SuhongChoe@users.noreply.github.com>
2023-10-25 17:49:08 +0900

    [Silver II] Title: 유기농 배추, Time: 108 ms, Memory: 13732 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,37 +1,40 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
+    static int[] parent;
+    static int n, m, k;
     static int[][] graph;
     static boolean[][] visited;
-    static int n, m, k;
+
+    static int cnt;
+
     static int[] dy = {0,0,1,-1};
     static int[] dx = {1,-1,0,0};
-    static int cntCycle;
 
-    public static void bfs(int start_y, int start_x){
-        cntCycle++;
+    public static void bfs(int _y, int _x){
+        Deque<int[]> q = new LinkedList<>();
+        q.addFirst(new int[] {_y, _x});
+        visited[_y][_x] = true;
 
-        Deque<int []> q = new LinkedList<>();
-        q.addFirst(new int[] {start_y, start_x});
-        visited[start_y][start_x] = true;
+        cnt++;
 
         while(!q.isEmpty()){
-            int[] node = q.pollLast(); // y, x
+            int[] node = q.pollLast();
 
             for(int i=0; i<4; i++){
                 int ny = dy[i] + node[0];
                 int nx = dx[i] + node[1];
 
-                if (0<= ny && ny < m && 0<= nx && nx < n && !visited[ny][nx] && graph[ny][nx]==1){
-                    q.addFirst(new int[] {ny, nx});
+                if(0 <= ny && ny < m && 0 <= nx && nx < n && !visited[ny][nx] && graph[ny][nx]==1){
                     visited[ny][nx] = true;
+                    q.addFirst(new int[] {ny, nx});
                 }
             }
         }
     }
-
-
+    
+    
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
@@ -42,7 +45,6 @@ public class Main {
 
         while(t-- > 0){
             st = new StringTokenizer(br.readLine());
-
             n = Integer.parseInt(st.nextToken());
             m = Integer.parseInt(st.nextToken());
             k = Integer.parseInt(st.nextToken());
@@ -50,26 +52,26 @@ public class Main {
             graph = new int[m][n];
             visited = new boolean[m][n];
 
-            for(int i=0; i<k; i++){
+            while(k-- > 0){
                 st = new StringTokenizer(br.readLine());
                 int x = Integer.parseInt(st.nextToken());
                 int y = Integer.parseInt(st.nextToken());
                 graph[y][x] = 1;
             }
 
-            cntCycle = 0;
-
+            cnt = 0;
             for(int i=0; i<m; i++){
                 for(int j=0; j<n; j++){
-                    if (graph[i][j]==1 && !visited[i][j]) {
+                    if(graph[i][j]==1 && !visited[i][j]){
                         bfs(i, j);
                     }
                 }
             }
 
-            sb.append(cntCycle).append("\n");
+            sb.append(cnt).append("\n");
         }
 
         System.out.print(sb);
+        
     }
 }
