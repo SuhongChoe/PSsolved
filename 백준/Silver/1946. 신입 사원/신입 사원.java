043621e36821c0f37db12e9b2fa94c2e1fa12a8a
Suhong Choe <57895652+SuhongChoe@users.noreply.github.com>
2023-10-27 23:01:19 +0900

    [Silver I] Title: 신입 사원, Time: 864 ms, Memory: 297628 KB -BaekjoonHub

---
+++ 백준/Silver/1946. 신입 사원/신입 사원.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int t = Integer.parseInt(br.readLine());
+
+        StringBuffer sb = new StringBuffer();
+        while(t-- > 0){
+            int n = Integer.parseInt(br.readLine());
+
+            int[] arr = new int[n+1];
+
+            for(int i=0; i<n; i++) {
+                st = new StringTokenizer(br.readLine());
+                arr[Integer.parseInt(st.nextToken())] = Integer.parseInt(st.nextToken());
+            }
+
+            /*
+            7 6 5 4 3 2 1
+            3 1 7 2 6 5 4
+             */
+
+            int cnt = 0;
+            int tmp = arr[1];
+
+            for(int i=2; i<=n; i++){
+                if(tmp<arr[i]){
+                    cnt++;
+                } else {
+                    tmp = arr[i];
+                }
+            }
+
+            sb.append(n-cnt).append("\n");
+        }
+        System.out.print(sb);
+    }
+}
