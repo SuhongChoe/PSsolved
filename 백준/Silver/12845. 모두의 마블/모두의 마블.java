88111d96f500cb600dcbfaa3cd132069fb94f4e3
Suhong Choe <57895652+SuhongChoe@users.noreply.github.com>
2023-10-27 23:44:53 +0900

    [Silver III] Title: 모두의 마블, Time: 84 ms, Memory: 11776 KB -BaekjoonHub

--- 백준/Silver/12845. 모두의 마블/모두의 마블.java
+++ 백준/Silver/12845. 모두의 마블/모두의 마블.java
@@ -7,20 +7,18 @@ public class Main {
         StringTokenizer st;
 
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
+
+        int sum=0;
+        int max=0;
+        int tmp;
 
         st = new StringTokenizer(br.readLine());
-        for(int i=0; i<n; i++) arr[i] = Integer.parseInt(st.nextToken());
-
-        Arrays.sort(arr);
-
-        int res=0;
-
-        for(int i=n-2; i>=0; i--){
-             res += arr[i]+arr[i+1];
-             arr[i] = arr[i+1];
+        for(int i=0; i<n; i++) {
+            tmp = Integer.parseInt(st.nextToken());
+            sum+=tmp;
+            if(max<tmp) max=tmp;
         }
-
-        System.out.print(res);
+        
+        System.out.print((sum-max)+max*(n-1));
     }
 }
