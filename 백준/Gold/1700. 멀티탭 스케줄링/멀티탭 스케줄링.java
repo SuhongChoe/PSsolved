e40ed3cd5f47c2780f7d9bde77402a607c04b3e2
Suhong Choe <57895652+SuhongChoe@users.noreply.github.com>
2023-11-03 14:32:09 +0900

    [Gold I] Title: 멀티탭 스케줄링, Time: 76 ms, Memory: 11616 KB -BaekjoonHub

--- 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.java
+++ 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.java
@@ -5,65 +5,61 @@ public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
 
-    static void next() throws IOException {
+    static void read() throws IOException{
         st = new StringTokenizer(br.readLine());
     }
 
-    static int nextInt(){
+    static int nextInt() {
         return Integer.parseInt(st.nextToken());
     }
 
-    public static int findElec(int i){
-        int target = 0;
-        boolean[] visited = new boolean[k+1];
+    static int n, k, res;
+    static int[] arr, num;
+    static HashSet<Integer> using = new HashSet<>();
 
+    public static int findElec(int i){
+        boolean[] visited = new boolean[k+1];
+        int target = 0;
+        
         for(int j=i+1; j<k; j++){
             if(using.contains(arr[j]) && !visited[arr[j]]){
                 visited[arr[j]] = true;
                 target = arr[j];
             }
         }
+
         for(int elec : using){
-            if(nums[elec]==0){
+            if(num[elec]==0){
                 target = elec;
             }
         }
-
+        
         return target;
     }
 
-    static int[] nums;
-    static int[] arr;
-    static int n, k;
-    static HashSet<Integer> using = new HashSet<>();
-
-
     public static void main(String[] args) throws IOException{
-        next();
+        read();
         n = nextInt();
         k = nextInt();
-
         arr = new int[k];
-        nums = new int[k+1];
+        num = new int[k+1];
 
-        next();
-        for(int i=0; i<k; i++){
+        read();
+        for(int i=0; i<k; i++) {
             arr[i] = nextInt();
-            nums[arr[i]]++;
+            num[arr[i]]++;
         }
 
-        int cnt=0;
+        int res = 0;
 
         for(int i=0; i<k; i++){
-            nums[arr[i]]--;
+            num[arr[i]]--;
             if(using.size()==n && !using.contains(arr[i])){
-                cnt++;
+                res++;
                 using.remove(findElec(i));
             }
             using.add(arr[i]);
         }
-
-        System.out.print(cnt);
-
+        System.out.print(res);
     }
 }
