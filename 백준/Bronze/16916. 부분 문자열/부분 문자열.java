e6b0c5d5f7ff1f0f5b1c2a67aa23eb3fab3a13a1
Suhong Choe <57895652+SuhongChoe@users.noreply.github.com>
2023-11-05 16:13:44 +0900

    [Bronze II] Title: 부분 문자열, Time: 240 ms, Memory: 34268 KB -BaekjoonHub

--- 백준/Bronze/16916. 부분 문자열/부분 문자열.java
+++ 백준/Bronze/16916. 부분 문자열/부분 문자열.java
@@ -1,50 +1,43 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    static String s, p;
-    static int[] kmp_table;
-
-    public static void kmp(String p){
-        kmp_table = new int[p.length()];
-
-        int i=0;
-        for(int j=1; j<p.length(); j++){
-            while(i>0 && p.charAt(i)!=p.charAt(j)){
-                i = kmp_table[i-1];
-            }
-            if(p.charAt(i)==p.charAt(j)){
-                i++;
-                kmp_table[j] = i;
-            }
-        }
-    }
-
-    public static void main(String[] args) throws IOException{
-        s = br.readLine();
-        p = br.readLine();
-
-        kmp(p);
-
-        int res=0;
-
-        int j=0;
-        for(int i=0;i<s.length();i++){
-            while(j>0 && s.charAt(i)!=p.charAt(j)){
-                j = kmp_table[j-1];
-            }
-            if(s.charAt(i)==p.charAt(j)){
-                j++;
-                if(j==p.length()){
-                    res = 1;
-                    break;
-                }
-
-            }
-        }
-
-        System.out.print(res);
-    }
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
+	
+	public static void main(String[] args) throws IOException {
+		
+		char[] S = br.readLine().toCharArray();
+		char[] P = br.readLine().toCharArray();
+		
+		int res = 0;
+		
+		int[] f = new int[P.length];
+		
+		int j = 0;
+		for(int i = 1; i < P.length; i++) {
+			while(j > 0 && P[j] != P[i]) j = f[j-1];
+			if(P[j] == P[i]) f[i] = ++j;
+		}
+		
+		j = 0;
+		for(int i = 0; i < S.length; i++) {
+			
+			
+			while(j > 0 && P[j] != S[i]) j = f[j-1];
+			if(P[j] == S[i])
+				j++;
+			
+			if(j == P.length) {
+				res = 1;
+				break;
+			}
+		}
+		
+		System.out.println(res);
+		
+	}
+	
 }
