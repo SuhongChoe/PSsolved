762de8ee723e9d567298ff996db94823a21b9c12
Suhong Choe <57895652+SuhongChoe@users.noreply.github.com>
2023-11-05 16:20:00 +0900

    [Bronze II] Title: 부분 문자열, Time: 232 ms, Memory: 33484 KB -BaekjoonHub

--- 백준/Bronze/16916. 부분 문자열/부분 문자열.java
+++ 백준/Bronze/16916. 부분 문자열/부분 문자열.java
@@ -1,43 +1,34 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringBuilder sb = new StringBuilder();
-	static StringTokenizer st;
-	
-	public static void main(String[] args) throws IOException {
-		
-		char[] S = br.readLine().toCharArray();
-		char[] P = br.readLine().toCharArray();
-		
-		int res = 0;
-		
-		int[] f = new int[P.length];
-		
-		int j = 0;
-		for(int i = 1; i < P.length; i++) {
-			while(j > 0 && P[j] != P[i]) j = f[j-1];
-			if(P[j] == P[i]) f[i] = ++j;
-		}
-		
-		j = 0;
-		for(int i = 0; i < S.length; i++) {
-			
-			
-			while(j > 0 && P[j] != S[i]) j = f[j-1];
-			if(P[j] == S[i])
-				j++;
-			
-			if(j == P.length) {
-				res = 1;
-				break;
-			}
-		}
-		
-		System.out.println(res);
-		
-	}
-	
+    static char[] s, p;
+    static int[] kmp_table;
+
+    public static int kmp(){
+        kmp_table = new int[p.length];
+
+        int j=0;
+        for(int i=1; i<p.length; i++){
+            while(j>0 && p[i]!=p[j]) j = kmp_table[j-1];
+            if(p[i]==p[j]) kmp_table[i] = ++j;
+        }
+        
+        j = 0;
+        for(int i=0;i<s.length;i++){
+            while(j>0 && s[i]!=p[j]) j = kmp_table[j-1];
+            if(s[i]==p[j]) j++;
+            if(j==p.length) return 1;
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) throws IOException{
+        s = br.readLine().toCharArray();
+        p = br.readLine().toCharArray();
+
+        System.out.print(kmp());
+    }
 }
