a9ed76a83b6db983b006b058fe0ad12bcc0d5946
Suhong Choe <57895652+SuhongChoe@users.noreply.github.com>
2023-11-06 17:55:37 +0900

    [Silver I] Title: 미로 탐색, Time: 92 ms, Memory: 12236 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,85 +1,68 @@
 import java.util.*;
 import java.io.*;
 
-class Node {
-    private int y;
-    private int x;
-    private int cnt;
-    Node(int y, int x, int cnt){
-        this.y = y;
-        this.x = x;
-        this.cnt = cnt;
-    }
-
-    public int getY(){
-        return y;
-    }
-    public int getX(){
-        return x;
-    }
-    public int getCnt(){
-        return cnt;
-    }
-
-}
-
 public class Main {
-    static boolean visited[][];
-    static char graph[][];
-    static int m;
-    static int n;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
 
-    static int dx[] = {0,0,1,-1};
-    static int dy[] = {1,-1,0,0};
+    static int[] dy = {1,-1,0,0};
+    static int[] dx = {0,0,1,-1};
 
-    public static int BFS(Node start){
-        Queue<Node> q = new LinkedList<>();
-
-        q.add(start);
-        visited[start.getY()][start.getX()] = true;
-
-        int res = 0;
-
-        while (!q.isEmpty()){
-           Node node = q.poll();
-
-           if (node.getY()==m-1 && node.getX()==n-1) {
-               res = node.getCnt();
-               break;
-           }
-
-           for(int i=0; i<4; i++){
-               int ny = node.getY() + dy[i];
-               int nx = node.getX() + dx[i];
-
-               if (0 <= nx && nx < n && 0 <= ny && ny < m && graph[ny][nx]=='1' && !visited[ny][nx]){
-                    q.add(new Node(ny, nx, node.getCnt()+1));
-                    visited[ny][nx] = true;
-               }
-
-           }
+    public static class Point {
+        int y;
+        int x;
+        int cnt = 0;
+        Point(int a, int b, int c){
+            y=a;
+            x=b;
+            cnt=c;
         }
+    }
 
+    public static int bfs(Point start){
+        Deque<Point> q = new LinkedList<>();
+        q.addFirst(start);
+        visited[start.y][start.x] = true;
+
+        int res=0;
+
+        while(!q.isEmpty()){
+            Point node = q.pollLast();
+            
+            if(node.x==n-1 && node.y==m-1){
+                res = node.cnt;
+                break;
+            }
+
+            for(int i=0; i<4; i++){
+                int ny = node.y + dy[i];
+                int nx = node.x + dx[i];
+                
+                if(0 <= nx && nx < n && 0 <= ny && ny < m && graph[ny][nx]=='1' && !visited[ny][nx]){
+                    q.addFirst(new Point(ny, nx, node.cnt+1));
+                    visited[ny][nx] = true;
+                }
+            }
+        }
+        
         return res;
     }
 
+    static int m, n;
+    static char[][] graph;
+    static boolean[][] visited;
     public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st =new StringTokenizer(br.readLine());
-
+        st = new StringTokenizer(br.readLine());
         m = Integer.parseInt(st.nextToken());
         n = Integer.parseInt(st.nextToken());
-
         graph = new char[m][n];
         visited = new boolean[m][n];
 
+        for(int i=0; i<m; i++) graph[i] = br.readLine().toCharArray();
 
-        for(int i=0; i<m; i++){
-            graph[i] = br.readLine().toCharArray();
-        }
-
-        int res = BFS(new Node(0,0,1));
+        int res = bfs(new Point(0, 0, 1));
 
         System.out.print(res);
+        
     }
 }
