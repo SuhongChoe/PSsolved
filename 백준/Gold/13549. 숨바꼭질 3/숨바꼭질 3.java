5ffed011e32cb2175e94210f14e596ecd4e9e902
Suhong Choe <57895652+SuhongChoe@users.noreply.github.com>
2023-11-08 13:57:46 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 136 ms, Memory: 20076 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -12,23 +12,22 @@ public class Main {
         Deque<Integer> q = new LinkedList<>();
         q.addFirst(n);
         visited[n] = 0;
-
+        
         while(!q.isEmpty()){
             int x = q.pollLast();
 
-            int next_x = x*2;
-            while(next_x<=100000 && visited[next_x]>visited[x]){
+            for(int next_x=x<<1; next_x > 0 && next_x<=100000 && visited[next_x] == 100000; next_x <<= 1){
                 visited[next_x] = visited[x];
-                q.add(next_x);
-                next_x *= 2;
+                q.addFirst(next_x);
             }
 
-            for(int next_x2 : new int[] {x-1, x+1}){
-                if(next_x2 >= 0 && next_x2<=100000 && visited[next_x2]>visited[x]+1){
-                    visited[next_x2] = visited[x] + 1;
-                    q.add(next_x2);
+            for(int next_x : new int[] {x-1, x+1}){
+                if(next_x >= 0 && next_x<=100000 && visited[next_x] == 100000){
+                    visited[next_x] = visited[x] + 1;
+                    q.addFirst(next_x);
                 }
             }
+            
         }
     }
     
@@ -40,10 +39,8 @@ public class Main {
         Arrays.fill(visited, 100000);
 
         bfs();
-
-        System.out.print(visited[k]);
         
-        // for(int i=0; i<=k; i++) System.out.print(visited[i] + " ");
+        System.out.print(visited[k]);
         
     }
 }
