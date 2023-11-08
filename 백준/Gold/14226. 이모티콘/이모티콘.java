ab605db3970803d3a5bbcc034d0a7bf855192aa7
Suhong Choe <57895652+SuhongChoe@users.noreply.github.com>
2023-11-08 16:17:16 +0900

    [Gold IV] Title: 이모티콘, Time: 96 ms, Memory: 17932 KB -BaekjoonHub

--- 백준/Gold/14226. 이모티콘/이모티콘.java
+++ 백준/Gold/14226. 이모티콘/이모티콘.java
@@ -20,20 +20,20 @@ public class Main {
     static void bfs(){
         Deque<Status> q = new LinkedList<>();
         q.addFirst(new Status(1, 0));
-        visited[1][0] = 0;
+        visited[1][0] = 1;
 
         while(!q.isEmpty()){
             Status x = q.pollLast();
 
             if(x.s==findS){
-                System.out.print(visited[x.s][x.c]);
+                System.out.print(visited[x.s][x.c]-1);
                 return;
             }
 
             for(int[] next_x : new int[][] {{x.s, x.s}, {x.s+x.c, x.c}, {x.s-1, x.c}}){
                 int s = next_x[0];
                 int c = next_x[1];
-                if(s >= 1 && s <= 1000 && c!=0 && visited[s][c]==1001){
+                if(s >= 1 && s <= 1000 && c!=0 && visited[s][c]==0){
                     visited[s][c] = visited[x.s][x.c] + 1;
                     q.addFirst(new Status(s, c));
                 }
@@ -44,11 +44,6 @@ public class Main {
     public static void main(String[] args) throws IOException{
         findS = Integer.parseInt(br.readLine());
         visited = new int[1001][1001];
-        for(int i=0; i<1001; i++){
-            for(int j=0; j<1001; j++){
-                visited[i][j] = 1001;
-            }
-        }
 
         bfs();
         
